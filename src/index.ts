dc8170a87c0a20afd0d85943e46ef9bb97469cce
asiwajudev <olanrewajuranda@gmail.com>
2022-03-01 21:11:24 +0100

    Configured routes and auth controller

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,13 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
+import { routes } from "./routes";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors({ origin: "http://localhost:3000" }));
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
-});
+routes(app);
 
 app.listen(8000, () => {
   console.log("Server is running on port 8000");
