0f9695c57379bd78b0b815f7d917eae10fdc0787
asiwajudev <olanrewajuranda@gmail.com>
2022-03-02 13:47:31 +0100

    Created User table using the entity property of TypeORM

--- src/index.ts
+++ src/index.ts
@@ -17,13 +17,3 @@ createConnection()
     });
   })
   .catch((error) => console.log(error));
-
-// const app = express();
-
-// app.use(express.json());
-
-// routes(app);
-
-// app.listen(8000, () => {
-//   console.log("Server is running on port 8000");
-// });
