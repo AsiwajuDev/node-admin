a9a46a893c8ec44869ee2b49ca2e7b3fbec7d1f9
asiwajudev <olanrewajuranda@gmail.com>
2022-03-02 13:27:43 +0100

    Configured TypeORM and connected to MySQL Database

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,29 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import { routes } from "./routes";
+import { createConnection } from "typeorm";
 
-const app = express();
+createConnection()
+  .then((connnection) => {
+    const app = express();
 
-app.use(express.json());
-app.use(cors({ origin: "http://localhost:3000" }));
+    app.use(express.json());
+    app.use(cors({ origin: "http://localhost:3000" }));
 
-routes(app);
+    routes(app);
 
-app.listen(8000, () => {
-  console.log("Server is running on port 8000");
-});
+    app.listen(3000, () => {
+      console.log("Server started on port 3000");
+    });
+  })
+  .catch((error) => console.log(error));
+
+// const app = express();
+
+// app.use(express.json());
+
+// routes(app);
+
+// app.listen(8000, () => {
+//   console.log("Server is running on port 8000");
+// });
